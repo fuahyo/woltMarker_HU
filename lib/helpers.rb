aca04b15477e6e0b842d8459b22560a61275d30b
ibnuthoriqh <ibnuthoriqh@gmail.com>
2022-09-01 08:40:41 +0700

    fix

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -5,16 +5,22 @@ module Helpers
                 "URL" => "https://wolt.com/en/cyp/nicosia/venue/wolt-market-nicosia",
                 "lang" => "en",
                 "store_id" => "62398fab17a025073829c9a3",
+                "language" => "ENG",
+                "currency_code_lc" => "EUR",
             },
             "HU" => {
                 "URL" => "https://wolt.com/en/hun/budapest/venue/wolt-market-nyugati",
                 "lang" => "en",
                 "store_id" => "61a0ab630e2c8131bf265e60",
+                "language" => "HUN",
+                "currency_code_lc" => "HUF",
             },
             "CZ" => {
                 "URL" => "https://wolt.com/en/cze/prague/venue/wolt-market-smichov",
                 "lang" => "en",
                 "store_id" => "61780a5b5777202f21085625",
+                "language" => "CZE",
+                "currency_code_lc" => "CZK",
             },
         }
     end
