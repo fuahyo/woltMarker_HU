cdf9cc7defbd869e2073623fc28a5b88bcffcd87
LitvinenkoD89 <torerolda@gmail.com>
2023-06-30 13:46:05 +0300

    add env var

--- parsers/product.rb
+++ parsers/product.rb
@@ -7,6 +7,6 @@ country_of_origin = html.css('div[class*="InfoItem_root"]').find{|s| s.text.incl
 
 
 outputs << vars['product'].merge({
-    "scraped_at_timestamp" => (ENV['reparse'] == "1" ? (Time.parse(page['fetched_at']) + 1).strftime('%Y-%m-%d %H:%M:%S') : Time.parse(page['fetched_at']).strftime('%Y-%m-%d %H:%M:%S')),
+    "scraped_at_timestamp" => ((ENV['needs_reparse'] == 1 || ENV['needs_reparse'] == "1") ? (Time.parse(page['fetched_at']) + 1).strftime('%Y-%m-%d %H:%M:%S') : Time.parse(page['fetched_at']).strftime('%Y-%m-%d %H:%M:%S')),
     "country_of_origin" => country_of_origin,
 })
